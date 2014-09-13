0d7fdbad382517a740bc69e9684b9c4a604e6cac
Khairil Yusof <khairil.yusof@gmail.com>
2014-09-13 23:36:33 +0800

    Fix up vocabulary and add terms from current idwfed.org. website.

--- idwf/thematic/vocabulary/thematic.py
+++ idwf/thematic/vocabulary/thematic.py
@@ -9,14 +9,72 @@ class thematic(grok.GlobalUtility):
     grok.name('idwf.thematic.thematic')
     grok.implements(IVocabularyFactory)
 
-    _terms = [{
-        'value': 'termvalue',
-        'title': 'Term Title',
-        'token': 'termtoken',
-    }]
+    _terms = [
+        {
+            'value': 'c189',
+            'title': 'C189 Ratification',
+        },
+        {
+            'value': 'child domestic workers',
+            'title': 'Child Domestic Workers',
+        },
+        {
+            'value': 'collective bargaining',
+            'title': 'Collective Bargaining',
+        },
+        {
+            'value': 'communication',
+            'title': 'Communication',
+        },
+        {
+            'value': 'hoursofwork',
+            'title': 'Days Off & Hours of work',
+        },
+        {
+            'value': 'dignity',
+            'title': 'Dignity of Domestic Workers',
+        },
+        {
+            'value': 'bills',
+            'title': 'Domestic Workers Bills',
+        },
+        {
+            'value': 'working conditions',
+            'title': 'Employment & Working Conditions',
+        },
+        {
+            'value': 'forced labour',
+            'title': 'Forced Labour',
+        },
+        {
+            'value': 'history',
+            'title': 'History',
+        },
+        {   'value': 'migrant',
+            'title': 'Migrant Domestic Workers',
+            },
+        {   'value': 'occupational safety',
+            'title': 'Occupational Safety & Health',
+            },
+        {   'value': 'research',
+            'title': 'Participatory Research',
+            },
+        {   'value': 'private agencies',
+            'title': 'Private Employment Agencies',
+            },
+        {   'value': 'organize',
+            'title': 'Rights to Organize',
+            },
+        {   'value': 'harassment',
+            'title': 'Sexual Harassment',
+            },
+        {   'value': 'worker rights',
+            'title': "Workers' Rights",
+            },
+        ]
 
     def __call__(self, context):
-        terms = [(u'child domestic workers'),_(u'Child Domestic Works')]
+        terms = []
         for i in self._terms:
             terms.append(SimpleTerm(**i))
         return SimpleVocabulary(terms)
